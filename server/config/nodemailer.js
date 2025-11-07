dfb3d89cbb8c7dbf33a03cb8c911291399aebee5
Shivansh Kushwaha <shivanshkushwaha0007@gmail.com>
2025-11-07 22:24:08 +0530

    Updated Nodemailer transporter config for Render SMTP

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -3,10 +3,13 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
-  //secure: false,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // ✅ Required for Render
   },
 });
 
