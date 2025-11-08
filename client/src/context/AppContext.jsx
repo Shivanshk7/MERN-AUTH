37ad74f2fdb60f84aae1abc25cda6dd98d2ce3f2
Shivansh Kushwaha <shivanshkushwaha0007@gmail.com>
2025-11-08 16:20:34 +0530

    Final production + Safari cookie fix

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,13 @@ export const AppContent = createContext();
 export const AppContextProvider = (props) => {
   axios.defaults.withCredentials = true;
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  //const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const backendUrl =
+    import.meta.env.MODE === "development"
+      ? ""
+      : "https://mean-auth-backend.onrender.com";
+
   const [isLoggedin, setIsLoggedin] = useState(false);
   const [userData, setUserData] = useState(false);
 
