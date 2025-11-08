b0dc885c10ae95b32f982bb588da4b9b4e50f0f5
Shivansh Kushwaha <shivanshkushwaha0007@gmail.com>
2025-11-08 22:31:50 +0530

    Fixed Navbar dropdown tap support for mobile and iPad

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -43,25 +43,31 @@ const Navbar = () => {
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.arc} alt="" className="w-28 sm:w-32" />
       {userData ? (
-        <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
-          {userData.name[0].toUpperCase()}
-          <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
-            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
-              {!userData.isAccountVerified && (
+        <div className="relative group" onClick={toggleMenu}>
+          <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group">
+            {userData.name[0].toUpperCase()}
+            <div
+              className={`absolute top-0 right-0 z-10 text-black rounded pt-10 ${
+                menuOpen ? "block" : "hidden"
+              } group-hover:block`}
+            >
+              <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+                {!userData.isAccountVerified && (
+                  <li
+                    onClick={sendVerificationOtp}
+                    className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
+                  >
+                    Verify email
+                  </li>
+                )}
                 <li
-                  onClick={sendVerificationOtp}
-                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer"
+                  onClick={logout}
+                  className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10"
                 >
-                  Verify email
+                  Logout
                 </li>
-              )}
-              <li
-                onClick={logout}
-                className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10"
-              >
-                Logout
-              </li>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
       ) : (
